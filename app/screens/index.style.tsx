0cce801fafd5a3e303a0d091acb211e118cc405b
MaziarTiari <maziar.tiari@gmail.com>
2020-04-25 15:01:31 +0200

    remove unused codes

--- app/screens/index.style.tsx
+++ app/screens/index.style.tsx
@@ -1,14 +1,5 @@
-import { StyleProp, ViewStyle, StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 import Color from "../utils/theme/color";
-
-export const StackNavScreenHeaderStyle: StyleProp<ViewStyle> = {
-    backgroundColor: Color.navBackground,
-}
-
-export const StackNavScreenHeaderStyle_ios: StyleProp<ViewStyle> = {
-    backgroundColor: Color.navBackground,
-    height: 100
-}
 
 export const styles = StyleSheet.create({
     stackNavScreenHeader: {
