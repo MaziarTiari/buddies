61955e06c15ab2867dc207ca4bcb1b41ffb277b8
MaziarTiari <maziar.tiari@gmail.com>
2020-05-05 05:01:55 +0200

    make it easier to understand

--- app/components/FeedList/FeedList.tsx
+++ app/components/FeedList/FeedList.tsx
@@ -1,27 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, Picker } from "react-native";
 import Color from "../../utils/theme/color";
 import Container from "../Container/Container";
 import { LanguageContext } from '../../../state-management-dev/LanguageContext'
 
 const FeedList = () => {
+    const lang = useContext(LanguageContext);
     return (
         <Container layout="screen_centered" style={{paddingTop:40}}>
-            <Text style={{ color: Color.Theme.secondaryText }}><Headline /></Text>
+            <Text style={{ color: Color.Theme.secondaryText }}>
+              {lang.translations.headline}
+            </Text>
             <LanguageSelector />
-            <GreetingTranslator element={<Greeting />}/>
+            <Text style={{ color: Color.Theme.secondaryText }}>
+              <Greeting />
+            </Text>
         </Container>
     );
 };
-
-interface Props{
-    element: JSX.Element
-}
-const GreetingTranslator = (Props: Props) => 
-    <Text style={{ color: Color.Theme.secondaryText }}>
-        {Props.element}
-    </Text>
-
 
 export const Greeting = () => (
     <LanguageContext.Consumer>
@@ -29,33 +25,24 @@ export const Greeting = () => (
     </LanguageContext.Consumer>
 );
   
-export const Headline = () => (
-    <LanguageContext.Consumer>
-      {contextValue => contextValue.translations.headline}
-    </LanguageContext.Consumer>
-);
-  
 export const LanguageSelector = () => {
-    return (
-      <LanguageContext.Consumer>
-        {contextValue => (
-          <Picker
-            selectedValue={contextValue.language}
-            style={{width:"100%", height:40, color:Color.Theme.primaryItem}}
-            onValueChange={event => {
-              contextValue.changeLanguage(event);
-            }}
-            itemStyle={{color: Color.Theme.secondaryText}}
-          >
-            {contextValue.availableLanguages.map((language, i) => (
-              <Picker.Item 
-                key={i} 
-                label={("select language: " + language).toUpperCase()} value={language} />
-            ))}
-          </Picker>
-        )}
-      </LanguageContext.Consumer>
-    );
+  const lang = useContext(LanguageContext);
+  return (
+    <Picker
+      selectedValue={lang.language}
+      style={{width:"100%", height:40, color:Color.Theme.primaryItem}}
+      onValueChange={event => {
+        lang.changeLanguage(event);
+      }}
+      itemStyle={{color: Color.Theme.secondaryText}}
+    >
+      {lang.availableLanguages.map((language, i) => (
+        <Picker.Item 
+          key={i} 
+          label={("select language: " + language).toUpperCase()} value={language} />
+      ))}
+    </Picker>
+  );
 };
 
 export default FeedList;
--- app/components/ProfileAbout/ProfileAbout.tsx
+++ app/components/ProfileAbout/ProfileAbout.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { Button } from "react-native";
+import React, { useContext } from "react";
+import { Button, Text } from "react-native";
 import Container from "../Container/Container";
+import { LanguageContext } from "../../../state-management-dev/LanguageContext";
+import Color from "../../utils/theme/color";
 
 const ProfileAbout = ({ navigation }: any) => {
+    const lang = useContext(LanguageContext);
     return (
         <Container layout="screen_centered">
+            <Text style={{ color: Color.Theme.secondaryText, marginVertical: 30 }}>
+                {lang.translations.headline}
+            </Text>
             <Button title="Friends" onPress={() => navigation.navigate("FriendList")} />
         </Container>
     );
