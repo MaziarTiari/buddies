32bde67ea1dadce5bf9d8bd5d956243ac9aa70cd
Nico Hofmann <nico.hofmann.2000@gmail.com>
2020-06-04 17:20:10 +0200

    Add: Activity to SessionContext

--- app/context/SessionContext/SessionContext.tsx
+++ app/context/SessionContext/SessionContext.tsx
@@ -1,31 +1,46 @@
 import React, { createContext, Component } from "react";
 import { ISessionContextState, sessionContextInitialState, ActionState } from "./stateFrame";
 import { users } from "../../dev/example_data/users";
-import { IUserProfile, IUser } from "../../models/User/User";
+import { IUser } from "../../models/User/User";
+import { IUserProfile } from "../../models/User/UserProfile";
+import { activities, IActivity } from "../../dev/example_data/fetchedActivityList";
 
 export const SessionContext = createContext(sessionContextInitialState);
 
 export class SessionContextProvider extends Component {
-    
+
     fetchProfile = (id: number) => {
         this.setState({ profile: users[id] }); // TODO get by API
     };
 
+    fetchActivity = (id: string) => {
+        this.setState({ activity: activities.find(a => a.id === id) }); // TODO get by API
+    };
+
     setActionState = (action: ActionState) => {
-        this.setState({actionState: action});
+        this.setState({ actionState: action });
     }
 
     setUser = (userIn: IUser) => {
-        this.setState({user: userIn});
+        this.setState({ user: userIn });
     }
 
+    setActivity = (activityIn: IActivity) => {
+        this.setState({ activity: activityIn });
+    };
+
     setUserProfile = (userProfileIn: IUserProfile) => {
-        this.setState({userProfile: userProfileIn})
-    }
+        this.setState({ userProfile: userProfileIn });
+    };
 
     saveProfile = (updatedProfile: IUserProfile) => {
         this.setState({ profile: updatedProfile });
         console.log("Save Profile with id: ", this.state.userProfile.id); // TODO save by API
+    };
+
+    saveActivity = (updatedActivity: IActivity) => {
+        this.setState({ activity: updatedActivity });
+        console.log("Save Activity with id: ", this.state.activity.id);
     };
 
     state: ISessionContextState = {
@@ -34,7 +49,10 @@ export class SessionContextProvider extends Component {
         setUserProfile: this.setUserProfile,
         fetchProfile: this.fetchProfile,
         saveProfile: this.saveProfile,
-        setActionState: this.setActionState
+        setActionState: this.setActionState,
+        fetchActivity: this.fetchActivity,
+        saveActivity: this.saveActivity,
+        setActivity: this.setActivity,
     };
 
     render() {
--- app/context/SessionContext/stateFrame.ts
+++ app/context/SessionContext/stateFrame.ts
@@ -1,5 +1,6 @@
 import { IUser } from '../../models/User/User'
 import { IUserProfile } from "../../models/User/UserProfile";
+import { IActivity } from '../../dev/example_data/fetchedActivityList';
 
 export type ActionState = "on_tour" | "on_edit";
 
@@ -7,22 +8,30 @@ export interface ISessionContextState {
     user: IUser;
     userProfile: IUserProfile;
     actionState: ActionState;
+    activity: IActivity;
     setActionState: (action: ActionState) => void;
     setUser: (user: IUser) => void;
     setUserProfile: (profile: IUserProfile) => void;
     saveProfile: (updatedProfile: IUserProfile) => void;
     fetchProfile: (id: number) => void;
+    setActivity: (activity: IActivity) => void;
+    fetchActivity: (id: string) => void;
+    saveActivity: (updatedActivity: IActivity) => void;
 }
 
 export const sessionContextInitialState: ISessionContextState = {
     userProfile: {
-        id:"",birthDate:0,city:"",firstname:"",lastname:"",sex:"",userId:"",username:""
+        id: "", birthDate: 0, city: "", firstname: "", lastname: "", sex: "", userId: "", username: ""
     },
     actionState: "on_tour",
-    user: {email:"", id:"", password:"", phone:"", salt:""},
+    user: { email: "", id: "", password: "", phone: "", salt: "" },
+    activity: { title: "", id: "", visibility: "friends", startDate: 0, ownerUserId: "", location: "", description: "", imageName: "" },
     setUser: () => console.warn("Function setUser() is not implemeted!"),
     setUserProfile: () => console.warn("Function setUserProfile() is not implemeted!"),
     saveProfile: () => console.warn("Function saveProfile() is not implemeted!"),
     fetchProfile: () => console.warn("Function fetchProfile() is not implemented!"),
     setActionState: () => console.warn("Function setActionState() is not implemented!"),
+    setActivity: () => console.warn("Function setActivity() is not implemented!"),
+    fetchActivity: () => console.warn("Function fetchActivity() ist not implemented!"),
+    saveActivity: () => console.warn("Function saveActivity() is not implemented!"),
 };
