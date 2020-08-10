1fd9a86326963cab4fee7832025ccea660dba1f6
Nico Hofmann <nico.hofmann.2000@gmail.com>
2020-08-10 11:57:08 +0200

    Add: Selection to pick images from camera or image library

--- app/components/ProfileGalery/ProfileGalery.tsx
+++ app/components/ProfileGalery/ProfileGalery.tsx
@@ -1,55 +1,79 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import Container from "../Container/Container";
 import { ThemeContext } from "../../context/ThemeContext/ThemeContext";
 import ActionButton from "../ActionButton/ActionButton";
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import Permissions from "expo-permissions";
 import { Image } from "react-native";
 
 const ProfileGalery = () => {
-    const theme = useContext(ThemeContext).theme;
-    
-    const getPermissionAsync = async () => {
-        if (Constants.platform?.ios) {
-          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-          if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-          }
-        }
-    };
-    
-    const [imageUrl, setImage] = useState(() => { 
-        getPermissionAsync();
-        return "";
-    });
 
-    const _pickImage = async () => {
-        try {
-          let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: false,
-            aspect: [4, 3],
-            quality: 1,
-          });
-          if (!result.cancelled) {
-            setImage(result.uri);
-          }
+	enum SelectOption { CAMERA, FILE };
 
-          console.log(result);
-        } catch (E) {
-          console.log(E);
-        }
-    };
+	const theme = useContext(ThemeContext).theme;
 
-    return (
-        <Container type="screen" layout="root">
-            <ActionButton text="+" onPress={_pickImage} />
-            {imageUrl !== "" && 
-                <Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
-            }
-        </Container>
-    );
+	const [childrenVisible, setChildrenVisible] = useState<boolean>(false);
+
+	const getPermissionAsync = async () => {
+		if (Constants.platform?.ios) {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+			if (status !== 'granted') {
+				alert('Sorry, we need camera roll permissions to make this work!');
+			}
+		}
+	};
+
+	const [imageUrl, setImage] = useState(() => {
+		getPermissionAsync();
+		return "";
+	});
+
+	const handleAddPressed = (): void => {
+		setChildrenVisible(!childrenVisible);
+	}
+
+	const handleChildPressed = async (selectOption: SelectOption) => {
+		const pickerOptions: ImagePicker.ImagePickerOptions = {
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: false,
+			allowsMultipleSelection: true
+		}
+		try {
+			let pickerResult: ImagePicker.ImagePickerResult;
+			switch (selectOption) {
+				case SelectOption.CAMERA:
+					pickerResult = await ImagePicker.launchCameraAsync(pickerOptions);
+					break;
+				case SelectOption.FILE:
+					pickerResult = await ImagePicker.launchImageLibraryAsync(pickerOptions);
+					break;
+			}
+			if (!pickerResult.cancelled) {
+				setImage(pickerResult.uri);
+			}
+			console.log(pickerResult);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<Container type="screen" layout="root">
+			<ActionButton
+				icon="plus"
+				onPress={handleAddPressed}
+				children={[
+					{ icon: "camera", onPress: () => handleChildPressed(SelectOption.CAMERA) },
+					{ icon: "file-image", onPress: () => handleChildPressed(SelectOption.FILE) },
+				]}
+				showChildren={childrenVisible}
+			/>
+			{imageUrl !== "" &&
+				<Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
+			}
+		</Container>
+	);
 };
 
 export default ProfileGalery;
