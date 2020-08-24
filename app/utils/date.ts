4237692ac7cc7dc7b1ea56194bf832321e08bf7c
Nico Hofmann <nico.hofmann.2000@gmail.com>
2020-08-24 16:18:37 +0200

    Add: Function getDateDiffString()

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -11,3 +11,16 @@ export const getDateRangeString = (startTime?: number, textBetween?: string, end
     textBetween = textBetween ? " " + textBetween + " " : "";
     return from + (until ? textBetween + until : "");
 }
+
+export const getDateDiffString = (startTime: number, endTime: number): string => {
+    const a = moment.unix(startTime);
+    const b = moment.unix(endTime);
+    const days = b.diff(a, "days");
+    const hours = b.diff(a, "hours") % 24;
+    const minutes = b.diff(a, "minutes") % 60;
+    return (
+        (days > 0 ? (days + "d ") : "") +
+        (hours > 0 ? (hours + "h ") : "") +
+        (minutes > 0 ? (minutes + "m") : "")
+    );
+}
